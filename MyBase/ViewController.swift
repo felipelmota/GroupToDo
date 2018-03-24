c86b5dd2fd1b75c5f58040fd73fc425a74baf0b8
Felipe Mota <felipelmota@gmail.com>
2018-03-24 13:45:41 -0300

    Initial commit

---
+++ MyBase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyBase
+//
+//  Created by Felipe Mota on 08/03/18.
+//  Copyright © 2018 Felipe Mota. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
